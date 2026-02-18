26e6df8de6b5ef21548ffee854c8febeba3e0ff6
Kavindu321 <109643514+Kavindu321@users.noreply.github.com>
2026-02-18 19:49:53 +0530

    Remove unused Laravel backend

--- backend-laravel/config/paths.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'notice_pdf' => base_path('data/notice_2026.pdf'),
-    'schools_csv' => base_path('data/schools.csv'),
-    'tamilschools_csv' => base_path('data/tamilschools.csv'),
-];
